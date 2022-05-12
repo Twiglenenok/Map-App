29e5af80a29d26d8fdbb5387a0451281dbc53c35
Twiglenenok <whaitway@yandex.ru>
2022-05-12 16:51:53 +0300

    welcome page completed

--- src/components/Pages/WelcomePage.tsx
+++ src/components/Pages/WelcomePage.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import videoBg from "../assets/videoBg.mp4"
+import { Link } from "react-router-dom";
 
 export const WelcomePage:React.FC = () => {
     return (
+        <div className="main-welcome">
+        <div className="content">
+                <h1>
+                    Open the wounder of MapClicking
+                </h1>
+                <Link className="start-btn" to="/main">Get Started</Link>
+            </div>
         <div className="welcome">
             <video  
-             loop autoPlay 
-             src={videoBg}
+              loop autoPlay
+              src={videoBg}
              />
+
+        </div>
         </div>
     )
 }
